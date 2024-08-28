6a49590c5eb8a2d857d82c17fa24fde90a7bd18a
Bernardo Gostenski Rodrigues <bernardogrodrigues@gmail.com>
2024-08-28 15:19:20 -0300

    Adicionando comentarios no desafio!

--- sprint04/Desafio/Etapa 03/stringHash.py
+++ sprint04/Desafio/Etapa 03/stringHash.py
@@ -1,7 +1,9 @@
 import hashlib
 
+# Recebe a string 
 stringTohash = input("Digite a string que deseja transformar em hash: ")
 
+# Gera o hash utilizando SHA-1
 gerarHash = hashlib.sha1(stringTohash.encode())
 
 print("O Hash da string fornecida é: ", gerarHash.hexdigest())
