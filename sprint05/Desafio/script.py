b29f13f8f71cf3360a4d05e372ae4d9ffc498ce9
Bernardo Gostenski Rodrigues <bernardogrodrigues@gmail.com>
2024-09-10 15:40:08 -0300

    Atualizando Desafio

--- sprint05/Desafio/script.py
+++ sprint05/Desafio/script.py
@@ -1,34 +1,40 @@
 import boto3
 
-s3_client = boto3.client('s3')
+session = boto3.Session(profile_name='982081093508_AdministratorAccess')
+s3_client = session.client('s3', region_name='us-east-1')  
 
 bucket_name = 'desafio-sprint05'
-file_key = 'dadosabertos_pos-graduacao_programas.csv'  
-expression = """
-SELECT * FROM s3object where ano = '2014' and NivelCursoPG = 'Doutorado'; 
+file_key = 'dadosabertos_pos-graduacao_programas.csv'
+
+sql_query = """
+SELECT * FROM s3object WHERE ano = '2014' AND NivelCursoPG = 'Doutorado'
 """
 
-
-# Chama o S3 Select para realizar a consulta
-response = s3_client.select_object_content(
-    Bucket=bucket_name,
-    Key=file_key,
-    ExpressionType='SQL',
-    Expression=expression,
-    InputSerialization={'CSV': {"FileHeaderInfo": "USE"}},
-    OutputSerialization={'CSV': {}}
-)
-
-# Imprime o resultado da consulta
-for event in response['Payload']:
-    if 'Records' in event:
-        # Decodifica o payload dos registros e imprime
-        print(event['Records']['Payload'].decode('utf-8'))
-
-    if 'Stats' in event:
-        # Imprime estatísticas se disponível
-        print("S3 Select statistics:", event['Stats'])
+def run_s3_select(bucket, key, query):
+    try:
+        response = s3_client.select_object_content(
+            Bucket=bucket,
+            Key=key,
+            ExpressionType='SQL',
+            Expression=query,
+            InputSerialization={
+                'CSV': {
+                    'FileHeaderInfo': 'USE',
+                    'FieldDelimiter': ';' 
+                }
+            },
+            OutputSerialization={'CSV': {}}
+        )
         
-    if 'End' in event:
-        # Sinaliza o fim da resposta
-        print("End of response")
+        for event in response['Payload']:
+            if 'Records' in event:
+                print(event['Records']['Payload'].decode('utf-8'))
+            if 'Progress' in event:
+                print("Progress:", event['Progress'])
+            if 'Stats' in event:
+                print("Stats:", event['Stats'])
+    
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+
+run_s3_select(bucket_name, file_key, sql_query)
