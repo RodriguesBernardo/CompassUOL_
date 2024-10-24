859f1ccab2979c9621f7d204bb167bd1f5cb0f18
Bernardo Gostenski Rodrigues <bernardogrodrigues@gmail.com>
2024-10-24 10:07:48 -0300

    Adicionando correção nos exercicios, adicionando comentarios no código do desafio!

--- sprint08/Desafio/mainCSV.py
+++ sprint08/Desafio/mainCSV.py
@@ -4,24 +4,32 @@ from awsglue.context import GlueContext
 from pyspark.sql.functions import col, when, lit
 from datetime import datetime
 
+# Inicializamos o SparkContext e Glue
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
+# Definimos variaves referentes a ano, mes e dia
 data_atual = datetime.now()
 ano = data_atual.strftime("%Y")
 mes = data_atual.strftime("%m")
 dia = data_atual.strftime("%d")
 
+# Definimos o caminho destino e o local do arquivo CSV
 caminhoArquivoSeries = "s3://sprint07/raw/Local/CSV/Movies/16/10/2024/movies.csv"
 caminhoDestinoSeries = f"s3://sprint07/trusted/Movies-CSV/{ano}/{mes}/{dia}/"
+# Le o arquivo CSV do filmes usando Spark, definindo cabeçalho e demais informações
 series_df = spark.read.option("delimiter", "|").csv(caminhoArquivoSeries, header=True, inferSchema=True)
+# Filtra os filmes que sejam do gênero Drama ou Romance 
 series_drama_romance = series_df.filter((col("genero") == "Drama") | (col("genero") == "Romance"))
+# Remove as duplicatas na coluna titulopincipal
 series_drama_romance = series_drama_romance.dropDuplicates(["titulopincipal"])
+# Substitui valores nulos ou invalidos na colula anoLancamento para Desconhecido
+for coluna in series_drama_romance.columns:
+    series_drama_romance = series_drama_romance.withColumn(
+        coluna,
+        when(col(coluna).isNull() | (col(coluna) == '') | (col(coluna) == '\\N'), lit('Desconhecido')).otherwise(col(coluna))
+    )
 
-series_drama_romance = series_drama_romance.withColumn(
-    "anoLancamento",
-    when(col("anoLancamento").isNull() | (col("anoLancamento") == ''), lit('Desconhecido')).otherwise(col("anoLancamento"))
-)
-
+# Escreve os dados em formato parquet, particinando por genero e anoLancamento
 series_drama_romance.write.mode("overwrite").partitionBy("genero", "anoLancamento").parquet(caminhoDestinoSeries)
--- sprint08/Desafio/mainJSON.py
+++ sprint08/Desafio/mainJSON.py
@@ -4,29 +4,38 @@ from awsglue.context import GlueContext
 from pyspark.sql.functions import array_contains, col, when, explode, lit
 from datetime import datetime
 
+# Inicializamos o SparkContext e o Glue
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
+# Definimos variaveis para armazenas as informações de horario
 data_atual = datetime.now()
 ano = data_atual.strftime("%Y")
 mes = data_atual.strftime("%m")
 dia = data_atual.strftime("%d")
 
+# Definimos variaveis para o caminho dos arquivos de leitura e destino
 caminhoArquivoJSON = "s3://sprint07/raw/TMDB/JSON/2024/10/18/movies_2.json"
 caminhoDestino = f"s3://sprint07/trusted/Movies_TMDb/{ano}/{mes}/{dia}/"
 
+# Usamos o Spark para fazer a leitura do arquivo JSON
 json_df = spark.read.json(caminhoArquivoJSON)
 
+# Explodimos o array genero em linhas individuais
 json_exploded = json_df.withColumn("genero", explode(col("genero")))
 
+# Filtramos os filmes que são do genero Drama ou Romance
 json_drama_romance = json_exploded.filter((col("genero") == "Drama") | (col("genero") == "Romance"))
 
+# Removemos duplicatas com base na coluna titulo prinicipal
 json_drama_romance = json_drama_romance.dropDuplicates(["tituloprincipal"])
 
+# Substituiomos valores nulos ou invalidos na coluna anoLancamento para Desconhecido
 json_drama_romance = json_drama_romance.withColumn(
     "anoLancamento",
     when(col("anoLancamento").isNull() | (col("anoLancamento") == ''), lit('Desconhecido')).otherwise(col("anoLancamento"))
 )
 
+# Escreve os dados no formato Parquet
 json_drama_romance.write.mode("overwrite").partitionBy("genero", "anoLancamento").parquet(caminhoDestino)
